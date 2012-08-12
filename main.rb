af82a2c7f91e29b1adc19d4f9fc389d393fb4142
Michael Westbom <mwestbom@us.ibm.com>
2012-08-11 23:10:25 -0400

    Implemented templating.  Added html5 boilerplate.
    That was fun.  Had to patch Haml!
    Not that haml itself was broken, just that the stable version in
    Rubygems couldn't recognize sass 3.2.0, which meant that haml dumped
    warnings into stderr every time it rendered a scss file.
    
    Beyond that, nothing fancy.  Ruthlessly stole some code from the
    reddis-io website to implement a layout/content template scheme.
    
    TODO: There is a POST handler in main.rb that isn't covered by tests.
    That's bad.  Its sole purpose is to satisfy my wanting to see POST in
    action so at least it's not something that's real functionality.
    However it's not useful code, so it shall get cut in the next commit.

--- main.rb
+++ main.rb
@@ -1,17 +1,66 @@
+require "sass"
 require "cuba"
+require "cuba/render"
+
+# Allow for an optional settings file.
+begin
+  require "lib/settings"
+rescue LoadError
+  Cuba.settings[:couch] = { :server => 'localhost', :port => 5984 }
+end
 
 Cuba.use Rack::Session::Cookie
+Cuba.plugin Cuba::Render
 
 Cuba.define do
+  # These functions are stolen straight from
+  # https://github.com/antirez/redis-io/blob/master/app.rb
+  def render(path, locals = {})
+    options = {
+      :format => :html5
+    }
+
+    super(path, locals, options)
+  end
+
+  def haml(template, locals = {})
+    layout(partial(template, locals))
+  end
+
+  def partial(template, locals = {})
+    render("views/#{template}.haml", locals)
+  end
+
+  def layout(content)
+    partial("layout", content: content)
+  end
+
   on get do
+    on "css", extension("css") do |file|
+      res.headers["Content-Type"] = "text/css; charset=utf-8"
+      res.write render("assets/css/#{file}.scss")
+    end
+
+    on "js" do
+      res.headers["Content-Type"] = "text/javascript; charset=utf-8"
+      on "libs", extension("js") do |file|
+        res.write File.read("assets/js/libs/#{file}.js")
+      end
+
+      on extension("js") do |file|
+        res.write File.read("assets/js/#{file}.js")
+      end
+    end
+
     on root do
-      res['Content-Type'] = "text/plain"
-      res.write(<<EOL
-Welcome to Mike's Ruby pastebin.  It does very little at the moment.
-In the near future it will accept POST requests to /.  If you POST
-a parameter called 'paste' it will generate a paste of the contents.
-EOL
-               )
+      res.write haml("index")
     end
   end
+
+  on post do
+    on root do
+      res.write req.params['paste'].to_s
+    end
+  end
+
 end
