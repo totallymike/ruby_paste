86ffc3b96216cd8d857456be9ce58add0f31b029
Michael Westbom <mwestbom@us.ibm.com>
2012-08-13 12:29:42 -0400

    Added JS tests.
    First is redundant to the CSS test, but the second one makes sure the
    libs path works as well.

--- test/unittest.rb
+++ test/unittest.rb
@@ -11,6 +11,14 @@ scope do
     get "css/style.css"
     assert last_response.body["Boilerplate"]
   end
+  test "JS" do
+    get "js/script.js"
+    assert last_response.body["Author"]
+  end
+  test "JSLibsWork" do
+    get "js/libs/modernizr-2.5.3.min.js"
+    assert last_response.body["Modernizr"]
+  end
   test "Settings" do
     assert_equal "localhost", Cuba.settings[:couch][:server]
   end
