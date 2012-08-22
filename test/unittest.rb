8dd7faef51a4bb3ddd3c91a7e16e77b26995a758
Michael Westbom <mwestbom@us.ibm.com>
2012-08-21 22:14:31 -0400

    Modified tests for readability.

--- test/unittest.rb
+++ test/unittest.rb
@@ -9,15 +9,15 @@ scope do
   end
   test "CSS" do
     get "css/style.css"
-    assert last_response.body["Boilerplate"]
+    assert last_response.body.include? "Boilerplate"
   end
   test "JS" do
     get "js/script.js"
-    assert last_response.body["Author"]
+    assert last_response.body.include? "Author"
   end
   test "JSLibsWork" do
     get "js/libs/modernizr-2.5.3.min.js"
-    assert last_response.body["Modernizr"]
+    assert last_response.body.include? "Modernizr"
   end
   test "Settings" do
     assert_equal "http://localhost:5984/default", Cuba.settings[:couch]
